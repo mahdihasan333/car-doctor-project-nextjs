4924fcbaa7817dfe8bc341a907f95c3d566014fd
popyakter121 <popy2000akter@gmail.com>
2025-01-31 16:09:21 +0500

    added services card

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,18 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+export const collectionNamesObj = {
+    servicesCollection : 'test_services',
+}
+
+export default function dbConnect(collectionName) {
+  const uri = process.env.MONGODB_URI;
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  return client.db(process.env.DB_NAME).collection(collectionName);
+}
